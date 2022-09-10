ccc99734821adb129b8d12405c643f8fa70f2d0d
carlos Arenas <carlos.acrif@gmail.com>
2022-09-10 21:43:48 +1200

    commit to main branch

--- Main.py
+++ Main.py
@@ -4,21 +4,23 @@ import json, time
 
 app = Flask(__name__)
 
-@app.route('/', methods['GET'])
+@app.route('/', methods=['GET'])
 def home_page():
-    data_set = {'Page': 'Home', 'Message': 'Succesfully loaded he home page', 'Timestamp':time.time()}
+    data_set = {'Page': 'Home', 'Message': 'Succesfully loaded he home page', 'Timestampon':time.time()}
     json_dump = json.dumps(data_set)
 
     return json_dump
 
 
-@app.route('/user', methods['GET'])
+@app.route('/user', methods=['GET'])
 def request_page():
-    user_query = str()
+    user_query = str(request.args.get('user')) # /user/?user=carlosadmin
 
-    #https://www.youtube.com/watch?v=5ZMpbdK0uqU&ab_channel=Indently 4:10min
-
-    data_set = {'Page': 'Home', 'Message': 'Succesfully loaded he home page', 'Timestamp':time.time()}
+    data_set = {'Page': 'Request', 'Message': f'Succesfully got the request for {user_query}', 'Timestamp':time.time()}
     json_dump = json.dumps(data_set)
 
     return json_dump
+
+
+if __name__ == '__main__':
+    app.run(port=7777)
