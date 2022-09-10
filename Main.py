f57e271df49494d86da3d0a56e7c6a642e2de23f
carlos Arenas <carlos.acrif@gmail.com>
2022-09-10 22:08:57 +1200

    second push

--- Main.py
+++ Main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def home_page():
-    data_set = {'Page': 'Home', 'Message': 'Succesfully loaded he home page test1', 'Timestampon':time.time()}
+    data_set = {'Page': 'Home', 'Message': 'Succesfully loaded he home page', 'Timestamp':time.time()}
     json_dump = json.dumps(data_set)
 
     return json_dump
